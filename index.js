37109b77a150f0b8ee25cb2c05f2c6afcbf44643
munsif12 <munsifalimisri69@gmail.com>
2021-04-28 21:18:43 -0700

    intPrep js file

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-class apple {
-  constructor(...words) {
-    this.words = words;
+// class apple {
+//   constructor(...words) {
+//     this.words = words;
+//   }
+//   print() {
+//     console.log(`$${this.words.join("$")}$`);
+//   }
+//   //==> =>
+// }
+// var apple1 = new apple("apple", "banana", "mango", "orange", "grapes"); // output : $apple$banana$mango$orange$grapes$
+// apple1.print();
+
+///async js interiew Prep
+function prom() {
+  try {
+    return new Promise((res, rej) => {
+      var data = false;
+      if (data) res("success");
+      else rej("failed");
+    });
+  } catch (error) {
+    console.log(error);
   }
-  print() {
-    console.log(`$${this.words.join("$")}$`);
-  }
-  //==> =>
 }
-var apple1 = new apple("apple", "banana", "mango", "orange", "grapes"); // output : $apple$banana$mango$orange$grapes$
-apple1.print();
+async function result() {
+  const res = await prom();
+  console.log(res);
+}
+result();
