2bc6b48d6617409556e87b3fd507145631a66bb0
munsif12 <munsifalimisri69@gmail.com>
2021-05-08 21:40:57 +0500

    Interview prep basic hooks code

--- tryReactApp/first-new-react-app/src/ContextGetter.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from "react";
-import { context } from "./App";
-function ContextGetter() {
-  const contextApi = useContext(context);
-  console.log(context);
-  return <div>{contextApi.name}</div>;
-}
-
-export default ContextGetter;
